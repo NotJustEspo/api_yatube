fcb29854eb63d398f5439f74229a66704625ff82
Espo <notjustespo@yandex.ru>
2023-11-29 10:18:06 +0300

    Added permissions.py in api

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,18 +1,9 @@
 from rest_framework import permissions, status, viewsets
 from rest_framework.response import Response
 
-from posts.models import Post, Group
-from .serializers import CommentSerializer, PostSerializer, GroupSerializer
-
-
-class IsAuthorPermission(permissions.BasePermission):
-    """
-    Пользовательское разрешение.
-    """
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
+from api.permissions import AuthorOrReadOnly
+from api.serializers import CommentSerializer, GroupSerializer, PostSerializer
+from posts.models import Group, Post
 
 
 class PostsViewSet(viewsets.ModelViewSet):
@@ -21,7 +12,7 @@ class PostsViewSet(viewsets.ModelViewSet):
     """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated, IsAuthorPermission]
+    permission_classes = [permissions.IsAuthenticated, AuthorOrReadOnly]
 
     def perform_create(self, serializer):
         """
@@ -63,7 +54,7 @@ class CommentsViewSet(viewsets.ModelViewSet):
     Класс для работы с комментами.
     """
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated, IsAuthorPermission]
+    permission_classes = [permissions.IsAuthenticated, AuthorOrReadOnly]
 
     def get_queryset(self):
         post_id = self.kwargs.get('post_id')
