8fc37b12fb2f8a1c1acfd1cd34c0cb8960886e96
Espo <notjustespo@yandex.ru>
2023-11-29 10:40:58 +0300

    Changes views.py after review-1

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import permissions, status, viewsets
 from rest_framework.response import Response
 
@@ -15,30 +16,7 @@ class PostsViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, AuthorOrReadOnly]
 
     def perform_create(self, serializer):
-        """
-        Переопределение метода при создании поста.
-        """
         serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        """
-        Переопределение метода для обновления поста.
-        """
-        try:
-            serializer.instance.author = self.request.user
-            super().perform_update(serializer)
-        except PermissionError:
-            raise Response(status=status.HTTP_403_FORBIDDEN)
-
-    def perform_destroy(self, instance):
-        """
-        Переопределение метода для удаления поста.
-        """
-        try:
-            instance.author = self.request.user
-            super().perform_destroy(instance)
-        except PermissionError:
-            raise Response(status=status.HTTP_403_FORBIDDEN)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
@@ -56,42 +34,15 @@ class CommentsViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated, AuthorOrReadOnly]
 
-    def get_queryset(self):
+    def get_object_new(self):
         post_id = self.kwargs.get('post_id')
-        post = Post.objects.get(pk=post_id)
+        return get_object_or_404(Post, pk=post_id)
+
+    def get_queryset(self):
+        post = self.get_object_new()
         queryset = post.comments.all()
         return queryset
 
     def perform_create(self, serializer):
-        """
-        Переопределение метода создания комментов.
-        """
-        post_id = self.kwargs.get('post_id')
-        post = Post.objects.get(pk=post_id)
+        post = self.get_object_new()
         serializer.save(author=self.request.user, post=post)
-
-    def perform_update(self, serializer):
-        """
-        Переопределение метода обновления коммента.
-        """
-        post_id = self.kwargs.get('post_id')
-        post = Post.objects.get(pk=post_id)
-        try:
-            serializer.instance.author = self.request.user
-            post = post
-            super().perform_update(serializer)
-        except PermissionError:
-            raise Response(status=status.HTTP_403_FORBIDDEN)
-
-    def perform_destroy(self, instance):
-        """
-        Переопределение метода удаления коммента.
-        """
-        post_id = self.kwargs.get('post_id')
-        post = Post.objects.get(pk=post_id)
-        try:
-            instance.author = self.request.user
-            post = post
-            super().perform_destroy(instance)
-        except PermissionError:
-            raise Response(status=status.HTTP_403_FORBIDDEN)
